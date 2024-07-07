77fd7cc5837993309ca55d2f2512222bd5ea7171
atishbhu <atishkushwaha148@gmail.com>
2024-07-07 14:48:31 +0530

    debouncing added

---
+++ Debounce/index.js
@@ -0,0 +1,20 @@
+const debounce = (fn, delay) => {
+  let timerId = null;
+  return function (text) {
+    clearTimeout(timerId);
+    timerId = timerId = setTimeout(() => {
+      fn(text);
+    }, delay);
+  };
+};
+
+function fn(text) {
+  console.log(text);
+}
+const delay = 2000;
+const debounceFn = debounce(fn, delay);
+
+const inputRef = document.querySelector("#input-search");
+inputRef.addEventListener("keyup", (e) => {
+  debounceFn(e.target.value);
+});
